8004ffef74afca2d0ea2b797df5e1f55161de4c9
CannedToes <mylesglanville@gmail.com>
2026-04-29 15:32:45 +0200

    ddclient

--- modules/services/ddclient/default.nix
+++ modules/services/ddclient/default.nix
@@ -1,10 +1,14 @@
 { self, inputs, ... }: {
-  flake.nixosModules.ddclient = { pkgs, lib, ... }: {
+  flake.nixosModules.ddclient = { pkgs, lib, config, ... }: {
     services.ddclient = {
       enable = true;
       interval = "5min";
       protocol = "cloudflare";
       username = "token";
+      passwordFile = config.sops.secrets."ddclient/cloudflare".path;
+      domains = [ "myles.onl" ];
+      zone = "myles.onl";
+      ssl = true;
     };
   };
 }

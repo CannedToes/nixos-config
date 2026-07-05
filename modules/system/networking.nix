b582990e27b3a00e1f537c66af70d984cc42755a
Myles Glanville <mylesglanville@gmail.com>
2026-07-05 11:54:21 +0200

    updated sops age keys

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -16,8 +16,9 @@
     };
     users.users.root.openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFmotcueJgrko7lBYUAYKilC8Z99T497yUkl9wgk+ayy myles@laptop"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIU1WdfeFe320nwVimkgjb+b7hDS6NL8Tvx8BdwuTYNn myles@wsl"
 			"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDWTEHF9/6EHtpPUqWNUu3pIU7fZ588I3uUKw8SvaHx+ myles@wsl"
+			"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMnCO5uSmLDtbBkhD/FU/Ftk71MLaNDtoatlzoKAzAM6 root@server"
+			"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID0EkvtpsTAhCuJ28vo3KD9nHTRo3I9Xfg/pijEcRKUo root@nixos"
     ];
   };
 }

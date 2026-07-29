d7bbe37c49793c1db86c05c609141993140e3667
Myles Glanville <mylesglanville@gmail.com>
2026-07-29 13:46:41 +0200

    mealie: add nginx reverse proxy

--- modules/services/mealie.nix
+++ modules/services/mealie.nix
@@ -1,8 +1,24 @@
-{ ... }: {
-	flake.nixosModules.mealie = { ... }: {
-		services.mealie = {
-			enable = true;
-			database.createLocally = true;
-		};
-	};
+{...}: {
+  flake.nixosModules.mealie = {...}: {
+    networking.firewall.allowedTCPPorts = [9000];
+    services.mealie = {
+      enable = true;
+      database.createLocally = true;
+    };
+    services.nginx.virtualHosts."mealie.myles.onl" = {
+      useACMEHost = "myles.onl";
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:9000";
+        proxyWebsockets = true;
+        extraConfig = ''
+          proxy_redirect off;
+          proxy_set_header Host $host;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header X-Forwarded-Proto $scheme;
+        '';
+      };
+    };
+  };
 }

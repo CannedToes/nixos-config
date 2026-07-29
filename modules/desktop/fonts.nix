3dd7e82885c2e2a352f7cb154b8651246df2925a
Myles Glanville <mylesglanville@gmail.com>
2026-07-29 19:10:49 +0200

    fonts: reduce font set

--- modules/desktop/fonts.nix
+++ modules/desktop/fonts.nix
@@ -1,23 +1,10 @@
 {...}: {
   flake.nixosModules.fonts = {pkgs, ...}: {
     fonts.packages = with pkgs; [
-      atkinson-hyperlegible
-      geist-font
-      ibm-plex
       liberation_ttf
-      maple-mono.NF
-      monaspace
       nerd-fonts.commit-mono
-      nerd-fonts.hack
-      nerd-fonts.jetbrains-mono
-      nerd-fonts.liberation
-      nerd-fonts.noto
-      nerd-fonts.sauce-code-pro
-      nerd-fonts.space-mono
-      noto-fonts
-      noto-fonts-cjk-sans
+      nerd-fonts.symbols-only
       noto-fonts-color-emoji
-      recursive
     ];
   };
 }

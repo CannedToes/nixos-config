92ead59828c147bae3bb759bb68d9852d6f2a412
Myles Glanville <mylesglanville@gmail.com>
2026-07-29 13:46:52 +0200

    networking: update ssh authorized keys and add ipv6 nameservers

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -3,7 +3,9 @@
     networking.firewall.enable = true;
     networking.nameservers = [
       "1.1.1.1"
-			"1.0.0.1"
+      "1.0.0.1"
+      "2606:4700:4700::1111"
+      "2606:4700:4700::1001"
     ];
 
     services.openssh = {
@@ -15,10 +17,8 @@
       };
     };
     users.users.root.openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFmotcueJgrko7lBYUAYKilC8Z99T497yUkl9wgk+ayy myles@laptop"
-			"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDWTEHF9/6EHtpPUqWNUu3pIU7fZ588I3uUKw8SvaHx+ myles@wsl"
-			"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMnCO5uSmLDtbBkhD/FU/Ftk71MLaNDtoatlzoKAzAM6 root@server"
-			"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID0EkvtpsTAhCuJ28vo3KD9nHTRo3I9Xfg/pijEcRKUo root@nixos"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDWTEHF9/6EHtpPUqWNUu3pIU7fZ588I3uUKw8SvaHx+ myles@wsl"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKYm0nn+5HZIhGaLEB/bORe8kZAwUBytMFJKwD7MDFcZ root@server"
     ];
   };
 }

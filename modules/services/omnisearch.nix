b0c93cb458f4469603321143b66589fa2c9f1dd0
Myles Glanville <mylesglanville@gmail.com>
2026-07-29 13:46:36 +0200

    omnisearch: add meta-search service

---
+++ modules/services/omnisearch.nix
@@ -0,0 +1,22 @@
+{self, inputs, ...}: {
+  flake.nixosModules.omnisearch = {...}: {
+
+    imports = [
+      inputs.omnisearch.nixosModules.default
+    ];
+
+    services.omnisearch.enable = true;
+
+    services.nginx = {
+      virtualHosts."search.myles.onl" = {
+        useACMEHost = "myles.onl";
+        forceSSL = true;
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:8087";
+          proxyWebsockets = true;
+        };
+      };
+    };
+
+  };
+}

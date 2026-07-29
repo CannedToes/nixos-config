7acc32a53d583b99db80b2ca188d1146788ee8c9
Myles Glanville <mylesglanville@gmail.com>
2026-07-29 13:46:41 +0200

    searx: update engines and search config

--- modules/services/searx.nix
+++ modules/services/searx.nix
@@ -21,20 +21,62 @@
 
         server = {
           secret_key = "$SECRET_KEY";
-          public_instance = true;
-          limiter = true;
-          image_proxy = true;
           method = "GET";
+          image_proxy = true;
         };
 
         ui = {
           default_locale = "en";
+          hotkeys = "vim";
+          url_formatting = "pretty";
+          theme_args.simple_style = "auto";
         };
+
+        plugins."searx.plugins.infinite_scroll.SXNGPlugin".active = true;
 
         search = {
           safe_search = 0;
           autocomplete = "duckduckgo";
+          autocomplete_min = 4;
+          default_lang = "en";
+          suspended_times = {
+            SearxEngineTooManyRequests = 600;
+            SearxEngineCaptcha = 3600;
+          };
         };
+
+        engines = [
+          {
+            name = "duckduckgo";
+            engine = "duckduckgo";
+            shortcut = "ddg";
+            disabled = false;
+          }
+          {
+            name = "google";
+            engine = "google";
+            shortcut = "go";
+            disabled = false;
+          }
+          {
+            name = "brave";
+            engine = "brave";
+            shortcut = "bv";
+            disabled = false;
+          }
+          {
+            name = "startpage";
+            engine = "startpage";
+            shortcut = "sp";
+            disabled = true;
+          }
+          {
+            name = "wikipedia";
+            engine = "wikipedia";
+            shortcut = "wp";
+            disabled = false;
+          }
+        ];
       };
 
       limiterSettings = {

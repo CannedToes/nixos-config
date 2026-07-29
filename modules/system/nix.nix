dedaaec8c8aead8a7424009554049defd9b58a25
Myles Glanville <mylesglanville@gmail.com>
2026-07-29 13:46:49 +0200

    nix: fix whitespace in nh config

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -7,12 +7,12 @@
       auto-optimise-store = true;
     };
 
-		programs.nh = {
-			enable = true;
-			clean.enable = true;
-			clean.extraArgs = "--keep-since 4d --keep 3";
-			flake = "/home/myles/.config/nixos";
-		};
+    programs.nh = {
+      enable = true;
+      clean.enable = true;
+      clean.extraArgs = "--keep-since 4d --keep 3";
+      flake = "/home/myles/.config/nixos";
+    };
 
     system.stateVersion = "26.05";
   };

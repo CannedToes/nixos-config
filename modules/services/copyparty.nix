2de27b31e5999a48eab0891791f30e026a90d98f
Myles Glanville <mylesglanville@gmail.com>
2026-07-29 13:46:36 +0200

    copyparty: add file sharing service

---
+++ modules/services/copyparty.nix
@@ -0,0 +1,46 @@
+{ inputs, ... }: {
+  flake.nixosModules.copyparty = { config, lib, pkgs, ... }: {
+    imports = [ inputs.copyparty.nixosModules.default ];
+
+    sops.secrets."copyparty/myles" = {
+      owner = "copyparty";
+    };
+
+    services.copyparty = {
+      enable = true;
+
+      settings = {
+        i = "127.0.0.1";
+        no-reload = true;
+        hist = "/var/cache/copyparty";
+        shr = "/s";
+        rproxy = -1;
+      };
+
+      accounts.myles = {
+        passwordFile = config.sops.secrets."copyparty/myles".path;
+      };
+
+      volumes = {
+        "/" = {
+          path = "/var/lib/copyparty/data";
+          access = {
+            rwmd = [ "myles" ];
+          };
+        };
+      };
+    };
+
+    services.nginx.virtualHosts."copyparty.myles.onl" = {
+      useACMEHost = "myles.onl";
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:3923";
+        proxyWebsockets = true;
+        extraConfig = ''
+          client_max_body_size 0;
+        '';
+      };
+    };
+  };
+}

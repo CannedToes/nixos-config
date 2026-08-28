fd46ccf73178d39360847251c625b6c5729c0f1e
Myles Glanville <mylesglanville@gmail.com>
2026-08-28 14:36:26 +0200

    i did something

--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -14,53 +14,20 @@
       sops
       users
 
-      # -- hardware --
-      graphics
-
       # -- programs --
       cli
       dev
-
-      # -- wslg --
-      fonts
 
       # -- wsl --
       inputs.nixos-wsl.nixosModules.default
 
       # -- host configuration --
-      ({pkgs, ...}: {
+      ({
+        lib,
+        pkgs,
+        ...
+      }: {
         sops.defaultSopsFile = ./secrets.yaml;
-
-        boot.extraModprobeConfig = "options kvm_amd nested=1";
-
-        systemd.services.kvm-wsl = {
-          description = "Load KVM modules and set /dev/kvm permissions";
-          wantedBy = ["multi-user.target"];
-          serviceConfig = {
-            Type = "oneshot";
-            RemainAfterExit = true;
-            ExecStart = pkgs.writeShellScript "load-kvm" ''
-              ${pkgs.kmod}/bin/modprobe kvm
-              ${pkgs.kmod}/bin/modprobe kvm_amd
-              chown root:kvm /dev/kvm
-              chmod 0660 /dev/kvm
-            '';
-          };
-        };
-
-        users.groups.kvm = {};
-        users.users.myles.extraGroups = ["kvm" "libvirtd"];
-
-        virtualisation.libvirtd = {
-          enable = true;
-          qemu = {
-            package = pkgs.qemu_kvm;
-            runAsRoot = true;
-            swtpm.enable = true;
-          };
-        };
-
-        programs.virt-manager.enable = true;
 
         networking.hostName = "wsl";
 
@@ -68,25 +35,26 @@
         networking.firewall.trustedInterfaces = ["lo"];
         networking.networkmanager.enable = false;
 
+        networking.nameservers = lib.mkForce [];
+
+        hardware.graphics.enable = lib.mkForce false;
+
         systemd.network.enable = false;
         systemd.services.systemd-resolved.enable = false;
         systemd.services.systemd-udevd.enable = false;
-
-        nixpkgs.overlays = [inputs.emacs-overlay.overlays.default];
-
-        programs.fuse = {
-          enable = true;
-        };
+        systemd.services.systemd-udev-trigger.enable = false;
+        services.udev.enable = false;
 
         wsl = {
           enable = true;
           defaultUser = "myles";
-          startMenuLaunchers = true;
+          startMenuLaunchers = false;
           wslConf = {
-            automount.enabled = false;
-            interop.appendWindowsPath = false;
-            network.generateResolvConf = false;
+            network.generateResolvConf = true;
           };
+          extraBin = [
+            {src = "${pkgs.coreutils}/bin/true";}
+          ];
         };
       })
     ];
--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -4,7 +4,6 @@
 
     nix.settings = {
       experimental-features = ["nix-command" "flakes"];
-      auto-optimise-store = true;
     };
 
     programs.nh = {

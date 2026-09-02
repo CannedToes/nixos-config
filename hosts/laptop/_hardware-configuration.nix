e4ee7657b5a9d8a6f91a7c84c03c5021b64ee0c5
Myles Glanville <mylesglanville@gmail.com>
2026-09-02 17:23:38 +0200

    fix old hardware configuration for laptop

--- hosts/laptop/_hardware-configuration.nix
+++ hosts/laptop/_hardware-configuration.nix
@@ -1,13 +1,11 @@
 {...}: {
   fileSystems."/" = {
-    device = "/dev/mapper/luks-201514f5-0c8d-4349-b652-f4a821e7c40b";
+    device = "/dev/disk/by-uuid/ae97aa2c-3926-4e63-8369-7133d1199542";
     fsType = "ext4";
   };
 
-  boot.initrd.luks.devices."luks-201514f5-0c8d-4349-b652-f4a821e7c40b".device = "/dev/disk/by-uuid/201514f5-0c8d-4349-b652-f4a821e7c40b";
-
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/FED8-C984";
+    device = "/dev/disk/by-uuid/68F4-0C9D";
     fsType = "vfat";
     options = ["fmask=0077" "dmask=0077"];
   };
--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -68,58 +68,8 @@ in {
 
         networking.hosts."192.168.1.158" = subdomains;
 
-        services = {
-          tlp = {
-            enable = true;
-            settings = {
-              CPU_BOOST_ON_AC = 1;
-              CPU_BOOST_ON_BAT = 0;
-              CPU_DRIVER_OPMODE_ON_AC = "active";
-              CPU_DRIVER_OPMODE_ON_BAT = "active";
-              CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
-              CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-              CPU_SCALING_GOVERNOR_ON_AC = "schedutil";
-              CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-              RUNTIME_PM_ON_AC = "on";
-              RUNTIME_PM_ON_BAT = "auto";
-              USB_AUTOSUSPEND = 1;
-              WIFI_PWR_ON_AC = "off";
-              WIFI_PWR_ON_BAT = "on";
-            };
-          };
-        };
-
         nix.settings.max-jobs = 8;
-
-        virtualisation.vmVariant = {
-          services.spice-vdagentd.enable = true;
-
-          virtualisation = {
-            diskImage = "./laptop.qcow2";
-            memorySize = 4096;
-            cores = 4;
-            qemu.options = [
-              "-spice"
-              "addr=127.0.0.1,port=5930,disable-ticketing=on"
-
-              "-vga"
-              "qxl"
-
-              "-device"
-              "virtio-serial-pci"
-              "-chardev"
-              "spicevmc,id=spicechannel0,name=vdagent"
-              "-device"
-              "virtserialport,chardev=spicechannel0,name=com.redhat.spice.0"
-            ];
-          };
-        };
       })
     ];
-  };
-
-  perSystem = {...}: {
-    packages.laptop = self.nixosConfigurations.laptop.config.system.build.toplevel;
-    packages.laptop-vm = self.nixosConfigurations.laptop.config.system.build.vm;
   };
 }
